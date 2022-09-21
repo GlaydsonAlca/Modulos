841b581f664656d20135f7557d441eb2084cc19c
Glaydson Araruna <gg.hilloshy@gmail.com>
2022-09-21 18:55:05 -0300

    tentativa-0

---
+++ Modulos/pessoas.js
@@ -0,0 +1,65 @@
+let pessoas = [
+    {
+        idade: 80,
+        opniao: 1
+    },
+    {
+        idade: 70,
+        opniao: 2
+    },
+    {
+        idade: 68,
+        opniao: 3
+    },
+    {
+        idade: 35,
+        opniao: 1
+    },
+    {
+        idade: 56,
+        opniao: 1
+    },
+    {
+        idade: 92,
+        opniao: 1
+    },
+    {
+        idade: 46,
+        opniao: 2
+    },
+    {
+        idade: 39,
+        opniao: 3
+    },
+    {
+        idade: 66,
+        opniao: 1
+    },
+    {
+        idade: 55,
+        opniao: 1
+    },
+    {
+        idade: 88,
+        opniao: 1
+    },
+    {
+        idade: 99,
+        opniao: 2
+    },
+    {
+        idade: 44,
+        opniao: 3
+    },
+    {
+        idade: 33,
+        opniao: 1
+    },
+    {
+        idade: 51,
+        opniao: 1
+    }
+]
+
+module.exports = pessoas
+
---
+++ ex2.js
@@ -0,0 +1,45 @@
+let Pessoas = require('./modulos/pessoas')
+
+let lista = {
+    mediaIdade: function (array) {
+        let media = 0;
+        let qtdOpina = 0;
+        let idadeSoma = 0;
+
+        for (i = 0; i < array.length; i++) {
+            var Aux = array[i];
+            if (Aux.opniao === 3) {
+                qtdOpina++;
+                idadeSoma += Aux.idade;
+            }
+        }
+        media = idadeSoma / qtdOpina;
+        return media
+    },
+
+    qtdRegular: function (array) {
+        let qtd = 0;
+        for (i = 0; i < array.length; i++) {
+            var Aux = array[i];
+            if (Aux.opniao === 1) {
+                qtd++
+            }
+        }
+        return qtd;
+    },
+
+    bomPercent: function (array) {
+        let qtd = 0;
+        for (i = 0; i < array.length; i++) {
+            var Aux = array[i];
+            if (Aux.opniao === 2) {
+                qtd++
+            }
+        }
+        return (qtd * 100) / array.length
+    }
+}
+
+console.log(lista.mediaIdade(Pessoas));
+console.log(lista.qtdRegular(Pessoas));
+console.log(lista.bomPercent(Pessoas));
